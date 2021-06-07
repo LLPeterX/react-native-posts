e81492916fb467e01261d4a81b7722c325e7abb9
Peter Lebedev <LPeter@mail.ru>
2021-06-07 15:50:23 +0300

    sample navigation using Stack

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Constants from 'expo-constants'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { AboutScreen } from '../screens/AboutScreen';
@@ -11,9 +12,10 @@ export const AppNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={MainScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-        <Stack.Screen name="Post" component={PostScreen} />
+        {/* <Stack.Screen name="Main" component={MainScreen} options={{title: Constants.manifest.name}}/> */}
+        <Stack.Screen name="Main" component={MainScreen} options={{title: "React Native demo: Blog"}}/>
+        <Stack.Screen name="About" component={AboutScreen} options={{title: "О программе"}}/>
+        <Stack.Screen name="Post" component={PostScreen} options={{title: "Запись"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {StyleSheet, View, Text, Button} from 'react-native'
 
-export const MainScreen = ({}) => {
+export const MainScreen = ({navigation}) => {
   return (
     <View style={styles.center}>
     <Text>MainScreen</Text>
+    <Button title="go to post" onPress={()=>navigation.navigate('Post')}/>
     </View>
   );
-
 };
 
 const styles = StyleSheet.create({
