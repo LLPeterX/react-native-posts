1ce165f2d7e425ff2aa93ab4bfb1e2363787c1e4
Peter Lebedev <LPeter@mail.ru>
2021-06-07 16:19:05 +0300

    stylize headers via screenOptions

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import Constants from 'expo-constants'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { AboutScreen } from '../screens/AboutScreen';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { THEME } from '../../theme'
+import { Platform } from 'react-native'
 
 
 export const AppNavigation = () => {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
+      <Stack.Navigator initialRouteName="Main"
+      screenOptions={
+        {
+          headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+          },
+          headerTintColor: Platform.OS === 'android' ? THEME.HEADER_TEXT_COLOR : THEME.MAIN_COLOR
+        }
+      }
+      >
         {/* <Stack.Screen name="Main" component={MainScreen} options={{title: Constants.manifest.name}}/> */}
         <Stack.Screen name="Main" component={MainScreen} options={{title: "React Native demo: Blog"}}/>
         <Stack.Screen name="About" component={AboutScreen} options={{title: "О программе"}}/>
