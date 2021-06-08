054e238344d3264c27ffcf6010f126f1b7f8ada8
Peter Lebedev <LPeter@mail.ru>
2021-06-08 19:54:16 +0300

    add photo button in header

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { StyleSheet, View, Text, Button, FlatList } from 'react-native'
 import { DATA } from '../data'
 import { Post } from '../components/Post'
+import {AppHeaderIcon} from '../components/AppHeaderIcon'
+import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = (post) => {
     navigation.navigate('Post', {postId: post.id, date: post.date});
   }
+  
+  useEffect(()=>navigation.setOptions({
+    headerTtile: "Мой блог", 
+    headerRight: ()=><HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item title="Сделать фото" iconName="camera" onPress={()=>console.log('Press photo')}/>
+    </HeaderButtons>
+  }),
+  []);
+  //navigation.setOptions({headerTtile: "Мой блог", headerRight: ()=><Text>ОГО!</Text>});
 
   return (
     <View style={styles.wrapper}>
