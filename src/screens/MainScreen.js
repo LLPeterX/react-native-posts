829782651fc3549ab30431c87bb925fb9b4a1c89
Peter Lebedev <LPeter@mail.ru>
2021-06-08 20:32:27 +0300

    add star to PostScreen to show is booked

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { StyleSheet, View, Text, Button, FlatList } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
 import { DATA } from '../data'
 import { Post } from '../components/Post'
 import { AppHeaderIcon } from '../components/AppHeaderIcon'
@@ -7,18 +7,23 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = (post) => {
-    navigation.navigate('Post', { postId: post.id, date: post.date });
+    navigation.navigate('Post', { postId: post.id, date: post.date, booked: post.booked });
   }
 
   // поскольку navigation.setOptions() нелзя использовать напрямую (конфликт
   // с рендером MainScreen), используем его в хуке
   // Но не лучше ли юзать options в Stack.Screen в AppNavigation?
   useEffect(() => navigation.setOptions({
-    headerTtile: "Мой блог",
-    headerRight: () =>
+    title: "Мой блог",
+    headerRight: () => 
       <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
         <Item title="Сделать фото" iconName="camera" onPress={() => console.log('Press photo')} />
-      </HeaderButtons>
+      </HeaderButtons>,
+    headerLeft: () => 
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item title="Меню" iconName="menu" onPress={() => console.log('Press menu')} />
+    </HeaderButtons>
+   
   }),
     []);
 
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, View, Text, Image, Button, ScrollView, Alert } from 'react-native'
 import { THEME } from '../../theme';
 import { DATA } from '../data';
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
 
 export const PostScreen = ({ navigation, route }) => {
   const postId = route.params.postId;
   const post = DATA.find(d => d.id === postId);
+
+  const iconName = post.booked ? "star" : "star-outline";
+
+  useEffect(() => navigation.setOptions({
+    headerRight: () =>
+      <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item title="Favorite" iconName={iconName} onPress={() => console.log('Press star')} />
+      </HeaderButtons>
+
+  }),
+    []);
 
   const removeHandler = () => {
     Alert.alert("Удаление", "Вы хотите удалить пост?",
