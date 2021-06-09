56b7025399bfac0aa5bac79604e9dbf148b2bbc8
Peter Lebedev <LPeter@mail.ru>
2021-06-09 13:43:11 +0300

    add bottom navigation on MainScreen

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -6,27 +6,53 @@ import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
 import { THEME } from '../../theme'
 import { Platform } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export const AppNavigation = () => {
-  const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main"
-      screenOptions={
-        {
-          headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
-          },
-          headerTintColor: Platform.OS === 'android' ? THEME.HEADER_TEXT_COLOR : THEME.MAIN_COLOR
+        screenOptions={
+          {
+            headerStyle: {
+              backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+            },
+            headerTintColor: Platform.OS === 'android' ? THEME.HEADER_TEXT_COLOR : THEME.MAIN_COLOR
+          }
         }
-      }
       >
-        <Stack.Screen name="Main" component={MainScreen} options={{title: "React Native demo: Blog"}}/>
-        <Stack.Screen name="About" component={AboutScreen} options={{title: "О программе"}}/>
-        <Stack.Screen name="Post" component={PostScreen} options={({route})=>({ title: `Пост #${route.params.postId} - ${new Date(route.params.date).toLocaleDateString()}` })}/>
+        <Stack.Screen name="Main" component={BottomNavigator} options={{ title: "React Native demo: Blog" }} />
+        <Stack.Screen name="About" component={AboutScreen} options={{ title: "О программе" }} />
+        <Stack.Screen name="Post" component={PostScreen} options={({ route }) => ({ title: `Пост #${route.params.postId} - ${new Date(route.params.date).toLocaleDateString()}` })} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+export const BottomNavigator = () => {
+  return (
+    <Tab.Navigator initialRouteName="All">
+      <Tab.Screen
+        name="All"
+        component={MainScreen}
+        options={
+          {
+            tabBarLabel: "Все",
+            tabBarIcon: () => (<Ionicons name="albums" size={24} color={THEME.MAIN_COLOR} />)
+          }}
+      />
+      <Tab.Screen
+        name="Booked"
+        component={MainScreen}
+        options={{
+          tabBarLabel: "Избранное",
+          tabBarIcon: () => <Ionicons name="heart-outline" size={24} color={THEME.MAIN_COLOR} />
+        }} />
+    </Tab.Navigator>
+  );
+}
