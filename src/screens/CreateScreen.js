c6758caeb9ab70843ea263ee971b301de25d48b5
Peter Lebedev <LPeter@mail.ru>
2021-06-14 14:05:28 +0300

    sample CreatePost screen - with input, image, buttons

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,21 +1,56 @@
-import React, {useEffect} from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Text, TextInput, Image, Button, ScrollView, Keyboard } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { THEME } from '../../theme'
 
 export const CreateScreen = ({ navigation, route }) => {
-  
-  useEffect(()=>navigation.setOptions({title: "Создать запись"}),[]);
+
+  // меняем заголовок в useEffect, т.к. сюда попадаем из разных мест из AppMavigation
+  useEffect(() => navigation.setOptions({ title: "Создать запись" }), []);
+
+  let [text, setText] = useState("");
+
+  const savePostHandler = () => {
+    console.log('Crate post!!!');
+  };
 
   return (
-    <View style={styles.center}>
-    <Text>Create Screen</Text>
-    </View>
+    <ScrollView>
+      <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Введите текст нового поста</Text>
+          <TextInput style={styles.textArea}
+            multiline={true}
+            placeholder="Введите текст нового поста..."
+            value={text}
+            onChangeText={setText}
+          />
+          {/* temp component for photo */}
+          <Image source={{ uri: 'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg' }} style={styles.image} />
+          <Button title="Создать пост" color={THEME.MAIN_COLOR} onPress={savePostHandler} />
+        </View>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  wrapper: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: 'open-bold',
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 10
+  },
+  textArea: {
+    padding: 10,
+    marginBottom: 10
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 20
   }
 });
